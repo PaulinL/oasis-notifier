edd773bdd870ec4096bef2dd38a1872eea47a9bd
Paulin Lambert <paulin.lambert@gmail.com>
2020-06-04 17:51:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Oasis notifier
+
+## Launch app
+### Configure environment variable
+```bash
+cp .env-sample .env
+```
+Then edit .env to add your values.
+
+### Install dependencies
+```bash
+npm install 
+```
+
+### Run app 
+```bash
+npm start
+```
---
+++ app.js
@@ -0,0 +1,95 @@
+const request = require('request');
+const NodeCache = require( "node-cache" );
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
+
+require('dotenv').config();
+
+const RECEIVERS = process.env.RECEIVERS;
+
+const cache = new NodeCache();
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.MAIL_ADDR,
+        pass: process.env.MAIL_PASSWD
+    }
+});
+
+const urls = {
+    login: "https://oasis.polytech.universite-paris-saclay.fr/prod/bo/core/Router/Ajax/ajax.php?targetProject=oasis_polytech_paris&route=BO\\Connection\\User::login",
+    marks: "https://oasis.polytech.universite-paris-saclay.fr/prod/bo/core/Router/Ajax/ajax.php?targetProject=oasis_polytech_paris&route=BO\\Layout\\MainContent::load&codepage=MYMARKS"
+}
+
+cron.schedule('* * * * *', makeRequest);
+
+function makeRequest() {
+    console.log("Logging in...")
+    request.post(urls.login, {
+        form: {
+            login: process.env.OASIS_LOGIN,
+            password: process.env.OASIS_PASSWD
+        }
+    }, afterLogin);
+}
+
+function afterLogin(error, response, body) {
+    console.log("Logged in");
+    const cookies = parseCookiesFromHeaders(response.headers);
+
+    request.get(urls.marks, {
+        headers: {
+            Cookie: cookiesToString(cookies)
+        }
+    }, afterGetMarks);
+}
+
+function notifyAboutNewMarks(number) {
+    console.log(number + " new mark(s).")
+    transporter.sendMail({
+        from: process.env.MAIL_ADDR,
+        to: RECEIVERS,
+        subject: number + " nouvelles notes !",
+        html: '<a href="https://oasis.polytech.universite-paris-saclay.fr/#codepage=MYMARKS">Accéder à Oasis</a>'
+    },  (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+function afterGetMarks(error, response, body) {
+    console.log("Got marks");
+    const oldNbMarks = cache.get("nbMarks");
+    const newNbMarks = body.match(/<a.*>Épreuves \((.*)\)<\/a>/)[1];
+    if(oldNbMarks && oldNbMarks !== newNbMarks) {
+        notifyAboutNewMarks(newNbMarks - oldNbMarks);
+    } else {
+        console.log("No new marks. Still " + newNbMarks + " marks.");
+    }
+    console.log("=============");
+    cache.set("nbMarks", newNbMarks);
+}
+
+function parseCookiesFromHeaders(headers) {
+    const cookies = {};
+    headers["set-cookie"].forEach(cookie => {
+        if(cookie.startsWith("PHPSESSID=")) {
+            cookies["PHPSESSID"] = cookie.match(/PHPSESSID=([a-z0-9]+);/)[1];
+        } else if (cookie.startsWith("bo_oasis_polytech_parisyear=") && !cookies["bo_oasis_polytech_parisyear"]) {
+            cookies["bo_oasis_polytech_parisyear"] = cookie.match(/bo_oasis_polytech_parisyear=(20[0-9]{2});/)[1];
+        }
+    });
+    return cookies;
+}
+
+function cookiesToString(cookies) {
+    let cookie = "";
+    for (let [key, value] of Object.entries(cookies)) {
+        cookie += `${key}=${value}; `
+    }
+    return cookie;
+}
